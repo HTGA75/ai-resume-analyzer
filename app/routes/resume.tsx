ba9f9437832d4e8c672ba116b2493e18ce882217
HTGA75 <hg23750@gmail.com>
2025-08-29 21:05:47 +0530

    setup Resume Review components to display score and improvements

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router'
+import ATS from '~/components/ATS'
+import Details from '~/components/Details'
+import Summary from '~/components/Summary'
 import { usePuterStore } from '~/lib/puter'
 
 export const meta = () => ([
@@ -75,10 +78,9 @@ const Resume = () => {
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                            {/* <Summary feedback={feedback} />
+                            <Summary feedback={feedback} />
                             <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
-                            <Details feedback={feedback} /> */}
-                            Summary ATS Details
+                            <Details feedback={feedback} />
                         </div>
                     ) : (
                         <img src="/images/resume-scan-2.gif" className="w-full" />
